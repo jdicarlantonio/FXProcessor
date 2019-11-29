fef0d889e0d27545f3ea04cf06a2654f35322096
jd <jd@0x29A>
2019-11-29 15:40:54 -0800

    input: front, output: mixing

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,83 +1,180 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #include "MainComponent.h"
 
-//==============================================================================
 MainComponent::MainComponent()
+: audioSetupComp (
+    deviceManager,
+    0,
+    256,
+    0,
+    256,
+    false,
+    false,
+    false,
+    false
+)
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
+    // set up gui
+    addAndMakeVisible(audioSetupComp);
+    addAndMakeVisible(diagnosticsBox);
 
-    // Some platforms require permissions to open input channels so request that here
-    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
-        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
-    {
-        RuntimePermissions::request (RuntimePermissions::recordAudio,
-                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
-    }
-    else
-    {
-        // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
-    }
+    diagnosticsBox.setMultiLine(true);
+    diagnosticsBox.setReturnKeyStartsNewLine(true);
+    diagnosticsBox.setReadOnly(true);
+    diagnosticsBox.setScrollbarsShown(true);
+    diagnosticsBox.setCaretVisible(false);
+    diagnosticsBox.setPopupMenuEnabled(true);
+    diagnosticsBox.setColour(TextEditor::backgroundColourId, Colour (0x32ffffff));
+    diagnosticsBox.setColour(TextEditor::outlineColourId,    Colour (0x1c000000));
+    diagnosticsBox.setColour(TextEditor::shadowColourId,     Colour (0x16000000));
+
+    cpuUsageLabel.setText("CPU Usage", dontSendNotification);
+    cpuUsageText.setJustificationType(Justification::right);
+    addAndMakeVisible(&cpuUsageLabel);
+    addAndMakeVisible(&cpuUsageText);
+
+    setSize(760, 360);
+
+    // audio device initialization
+    setAudioChannels(2, 2);
+    deviceManager.addChangeListener(this);
+
+    startTimer(50);
 }
 
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
+    deviceManager.removeChangeListener(this);
     shutdownAudio();
 }
 
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    // so far nothing to do here ...
 }
 
 void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
+    // get current device
+    auto* device = deviceManager.getCurrentAudioDevice();
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+    auto activeInputChannels = device->getActiveInputChannels();
+    auto activeOutputChannels = device->getActiveOutputChannels();
 
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    auto maxInputChannels = activeInputChannels.countNumberOfSetBits();
+    auto maxOutputChannels = activeOutputChannels.countNumberOfSetBits();
+
+    // handle processing and what not
+    for(auto channel = 0; channel < maxOutputChannels; ++channel)
+    {
+        if((!activeOutputChannels[channel]) || maxInputChannels == 0) 
+        {
+            bufferToFill.buffer->clear(channel, bufferToFill.startSample, bufferToFill.numSamples);
+        }
+        else
+        {
+            auto actualInputChannel = channel % maxInputChannels;
+
+            if(!activeInputChannels[channel])
+            {
+                bufferToFill.buffer->clear(channel, bufferToFill.startSample, bufferToFill.numSamples);
+            }
+            else
+            {
+                auto* input = bufferToFill.buffer->getReadPointer(actualInputChannel, bufferToFill.startSample);
+                auto* output = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+
+                for(auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+                {
+                    output[sample] = input[sample];
+                }
+            }
+        }
+    }
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
 //==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    g.fillRect(getLocalBounds().removeFromRight(proportionOfWidth(0.4f)));
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    auto rect = getLocalBounds();
+
+    audioSetupComp.setBounds(rect.removeFromLeft(proportionOfWidth(0.6f)));
+    rect.reduce(10, 10);
+
+    auto topLine(rect.removeFromTop(20));
+    cpuUsageLabel.setBounds(topLine.removeFromLeft(topLine.getWidth() / 2));
+    cpuUsageText.setBounds(topLine);
+    rect.removeFromTop(20);
+
+    diagnosticsBox.setBounds(rect);
+}
+
+void MainComponent::changeListenerCallback(ChangeBroadcaster*)
+{
+    // device changed; show new info
+    dumpDeviceInfo();
+}
+
+String MainComponent::getListOfActiveBits(const BigInteger& b)
+{
+    StringArray bits;
+
+    for(auto i = 0; i <= b.getHighestBit(); ++i)
+    {
+        if(b[i])
+        {
+            bits.add(String(i));
+        }
+    }
+
+    return bits.joinIntoString(", ");
+}
+
+void MainComponent::timerCallback()
+{
+    auto cpu = deviceManager.getCpuUsage() * 100;
+    cpuUsageText.setText(String(cpu, 6) + " %", dontSendNotification);
+}
+
+void MainComponent::dumpDeviceInfo()
+{
+    logMessage("------------------------------------------");
+    logMessage(
+        "Current audio device type: " 
+        + (deviceManager.getCurrentDeviceTypeObject() != nullptr
+        ? deviceManager.getCurrentDeviceTypeObject()->getTypeName()
+        : "<none>")
+    );
+
+    if(auto* device = deviceManager.getCurrentAudioDevice())
+    {
+        logMessage("Current audio device: " + device->getName().quoted());
+        logMessage("Sample rate: " + String(device->getCurrentSampleRate()) + " Hz");
+        logMessage("Block size: " + String(device->getCurrentBufferSizeSamples()) + " samples");
+        logMessage("Bit depth: " + String(device->getCurrentBitDepth()));
+        logMessage("Input channel names: " + device->getInputChannelNames().joinIntoString(", "));
+        logMessage("Active input channels: " + getListOfActiveBits(device->getActiveInputChannels()));
+        logMessage("Output channel names: " + device->getOutputChannelNames().joinIntoString(", "));
+        logMessage("Active output channels: " + getListOfActiveBits(device->getActiveOutputChannels()));
+    }
+    else
+    {
+        logMessage("No audio device open");
+    }
+}
+
+void MainComponent::logMessage(const String& m)
+{
+    diagnosticsBox.moveCaretToEnd();
+    diagnosticsBox.insertTextAtCaret(m + newLine);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,40 +1,37 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent   : public AudioAppComponent
+class MainComponent   
+    : public AudioAppComponent
+    , public ChangeListener
+    , private Timer
 {
 public:
-    //==============================================================================
     MainComponent();
     ~MainComponent();
 
-    //==============================================================================
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    //==============================================================================
     void paint (Graphics& g) override;
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    void changeListenerCallback(ChangeBroadcaster*) override;
+    static String getListOfActiveBits(const BigInteger& b);
+    void timerCallback() override;
+    void dumpDeviceInfo();
+    void logMessage(const String& m);
 
+private:
+    AudioDeviceSelectorComponent audioSetupComp; // for allowing choice of device
+
+    // diagnostic information
+    Label cpuUsageLabel;
+    Label cpuUsageText;
+    TextEditor diagnosticsBox;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
